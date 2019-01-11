0bb58c62a3b2a3be3d6d92d472b202e3853993f2
Karl Groves <karl@karlgroves.com>
2019-01-11 09:28:11 -0500

    Update deprecated package

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var unirest = require('unirest');
 var config = require('./config.json');
-var GitHubApi = require("github");
+const octokit = require('@octokit/rest')()
 
 var Request = unirest.post(config.tenon.tenonInstanceDomain + '/api/index.php')
   .send({
@@ -9,20 +9,12 @@ var Request = unirest.post(config.tenon.tenonInstanceDomain + '/api/index.php')
     url: 'http://www.google.com' // In the real world you're going to want to pass each url in some other way
   }).end(function(response) {
 
-    var github = new GitHubApi({
-      version: config.github.version,
-      debug: config.github.debug,
-      protocol: config.github.protocol,
-      host: config.github.host,
-      pathPrefix: config.github.pathPrefix,
-      timeout: config.github.timeout
-    });
 
-    github.authenticate({
-      type: "basic",
-      username: config.github.user,
-      password: config.github.pass
-    });
+  octokit.authenticate({
+    type: 'token',
+    token: config.github.token
+  });
+
 
     console.log('Tenon Status:');
     console.log(response.body.status);
@@ -39,21 +31,22 @@ var Request = unirest.post(config.tenon.tenonInstanceDomain + '/api/index.php')
     if (response.body.status === 200) {
 
       var fullIssueDescription = config.github.issueDescriptionPre + '\n' +
-        'Density: ' + response.body.resultSummary.density.allDensity + '\n' +
+        'Density: ' + response.body.resultSummary.density.allDensity + '%\n' +
         'Total Issues: ' + response.body.resultSummary.issues.totalIssues + '\n' +
         'Level A Issues: ' + response.body.resultSummary.issuesByLevel.A.count + '\n' +
         'Level AA Issues: ' + response.body.resultSummary.issuesByLevel.AA.count + '\n' +
         'Level AAA Issues: ' + response.body.resultSummary.issuesByLevel.AAA.count + '\n' +
-        'More at: ' + config.tenon.tenonInstanceDomain + '/history.php?responseID=' + response.body.request.responseID + '\n';
+        'See the full response at: ' + config.tenon.tenonInstanceDomain + '/history.php?responseID=' + response.body.request.responseID + '\n';
 
       var issue = {
         user: config.github.user,
         repo: config.github.repo,
+        owner: config.github.owner,
         title: response.body.resultSummary.issues.totalIssues + ' ' + config.github.issueSummaryPre + ' ' + response.body.request.url,
         body: fullIssueDescription
       };
 
-      github.issues.create(issue, function(err, response) {
+      octokit.issues.create(issue, function(err, response) {
         console.log(JSON.stringify(response, null, 4));
       });
     }
